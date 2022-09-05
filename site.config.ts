de600639b2aac21c0f738ef1326ca116f4a221e9
zhongzh <zhongzh05@outlook.com>
2022-09-05 11:54:52 +0800

    和源码同步

---
+++ site.config.ts
@@ -0,0 +1,68 @@
+import { siteConfig } from './lib/site-config'
+
+export default siteConfig({
+ // where it all starts -- the site's root Notion page (required)
+  rootNotionPageId: '96452d4a5dda4ed5a83c64ea1b38859f',
+
+  // if you want to restrict pages to a single notion workspace (optional)
+  // (this should be a Notion ID; see the docs for how to extract this)
+  rootNotionSpaceId: null,
+
+  // basic site info (required)
+  name: '记录四季',
+  domain: 'https://pattern-settee-735.notion.site/96452d4a5dda4ed5a83c64ea1b38859f',
+  author: 'zhongzh',
+
+  // open graph metadata (optional)
+  description: '记录四季',
+  socialImageTitle: 'Transitive Bullshit',
+  socialImageSubtitle: 'Hello World! ??',
+
+  // social usernames (optional)
+  twitter: '',
+  github: '',
+  linkedin: '',
+
+  // default notion icon and cover images for site-wide consistency (optional)
+  // page-specific values will override these site-wide defaults
+  defaultPageIcon: null,
+  defaultPageCover: null,
+  defaultPageCoverPosition: 0.5,
+
+  // image CDN host to proxy all image requests through (optional)
+  // NOTE: this requires you to set up an external image proxy
+  imageCDNHost: null,
+
+  // Utteranc.es comments via GitHub issue comments (optional)
+  utterancesGitHubRepo: null,
+
+  // whether or not to enable support for LQIP preview images (optional)
+  // NOTE: this requires you to set up Google Firebase and add the environment
+  // variables specified in .env.example
+  isPreviewImageSupportEnabled: false,
+
+  // map of notion page IDs to URL paths (optional)
+  // any pages defined here will override their default URL paths
+  // example:
+  //
+  // pageUrlOverrides: {
+  //   '/foo': '067dd719a912471ea9a3ac10710e7fdf',
+  //   '/bar': '0be6efce9daf42688f65c76b89f8eb27'
+  // }
+  pageUrlOverrides: null,
+
+  // whether to use the default notion navigation style or a custom one with links to
+  // important pages
+  navigationStyle: 'default'
+  // navigationStyle: 'custom',
+  // navigationLinks: [
+  //   {
+  //     title: 'About',
+  //     pageId: 'f1199d37579b41cbabfc0b5174f4256a'
+  //   },
+  //   {
+  //     title: 'Contact',
+  //     pageId: '6a29ebcb935a4f0689fe661ab5f3b8d1'
+  //   }
+  // ]
+})
